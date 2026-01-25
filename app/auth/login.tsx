5ab44f98ff0f5894ba7293d35fbfbbceeb8394ef
Waqas106 <m.waqas987321@gmail.com>
2026-01-25 21:56:19 +0500

    Updated login

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -14,28 +14,40 @@ export default function Login(){
         console.log(data.email, data.password);
     };
 return(
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <div className="flex items-center justify-center min-h-screen bg-slate-50">
+        <div className="w-full max-w-md rounded-md bg-white shadow-lg p-6">
 
-        <input 
-        type="email"
-        {...register("email")}
-        placeholder="email"
-        />
+            <div className="text-center py-4">
+                <h2 className="text-2xl font-bold">Welcome Back</h2>
+                <p className="text-gray-600">Sign in to your real estate platform account</p>
+            </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+    
+            <div>
+                <label className="block font-medium font-bold mb-2">Email Address</label>
+            <input type="email" {...register("email", {required: true})}placeholder="Enter your email"
+            className="px-6 py-2 border-2 border-gray-300 rounded-lg w-full focus:border-blue-500 focus:outline-none"
+            />
+            </div>
+    
+            <div>
+                <label className="block font-medium font-bold mb-2">Password</label>
+            <input 
+            type="password" {...register("password", {required: true})} placeholder="Enter your password"
+            className="px-6 py-2 border-2 border-gray-300 rounded-lg w-full focus:border-blue-500 focus:outline-none"
+            />
+            </div>
+    
+            <button type="submit">
+                Login
+    
+            </button>
+            
+    
+        </form>
+        </div>
 
-        <input 
-        type="password"
-        {...register("password")}
-        placeholder="password"
-        />
-
-        <button type="submit">
-            Login
-
-        </button>
-        
-
-    </form>
-
+    </div>
 )
 
 }
